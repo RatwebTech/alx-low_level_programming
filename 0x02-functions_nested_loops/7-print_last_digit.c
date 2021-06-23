5c7b6c7704fd0b6f663c8c072bfedae20245df42
root <athowezlie@gmail.com>
2021-06-23 06:43:45 -0700

    There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *print_last_digit - Prints the last digit
+ *of a number
+ *
+ *@y: input number is an integer.
+ *
+ *Return: last digit.
+ */
+int print_last_digit(int y)
+{
+int l;
+
+l = y % 10;
+if (1 < 0)
+{
+_putchar(-l + 48);
+return (-l);
+}
+else
+{
+_putchar(l + 48);
+return (l);
+}
+}
