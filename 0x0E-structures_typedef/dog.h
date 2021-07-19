fa8af7c5f122d9dcd6de8ca754ca7cd1fa95b96e
root <athowezlie@gmail.com>
2021-07-19 12:46:47 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/**
+ * struct dog - It's a dog, what can I say
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner name
+ *
+ * Description: It's a dog, what can I say
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
