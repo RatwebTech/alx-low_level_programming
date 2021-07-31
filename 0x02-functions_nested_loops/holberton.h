2a12a52deaa6311d712dc162a948d77c46fd3a3f
root <athowezlie@gmail.com>
2021-07-31 08:31:17 -0700

    holberton

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -15,17 +15,17 @@
  * print_to_98 - prints all natural numbers to 98
  * print_times_table - print times table up to
  **/
+
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
+int _islower(int a);
+int _isalpha(int z);
+int print_sign(int x);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int a, int b);
+int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
-void num(int r, int n);
+void print_times_table(int g);
