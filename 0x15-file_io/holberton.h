71d06d24320de60f8ff0a545483e42fda4816235
root <athowezlie@gmail.com>
2021-08-28 09:29:13 -0700

    task 1

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,13 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#include "main.h"
+
+#endif
